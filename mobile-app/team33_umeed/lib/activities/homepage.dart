154a574cb5bfb41d29dab4a673540d61592820cd
Akash Jindal <akashjindal347@gmail.com>
2020-07-26 01:54:41 +0530

    improvised dashboard

--- mobile-app/team33_umeed/lib/activities/homepage.dart
+++ mobile-app/team33_umeed/lib/activities/homepage.dart
@@ -40,56 +40,86 @@ class HomeState extends State<HomeActivity> {
     AuthUser authUser = Provider.of<AuthUser>(context);
     print(authUser.uId);
     return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.add),
+        ),
         appBar: AppBar(
           title: Text('Umeed'),
-          actions: <Widget>[
-            IconButton(icon: Icon(Icons.exit_to_app), onPressed: () async {
-                  await AuthService().signOut();
-                },
-                )
-          ],
+          // actions: <Widget>[
+          //   IconButton(
+          //     icon: Icon(Icons.exit_to_app),
+          //     onPressed: () async {
+          //       await AuthService().signOut();
+          //     },
+          //   )
+          // ],
+          elevation: 0,
         ),
         drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('Drawer Header'),
-              decoration: BoxDecoration(
-                color: Colors.purple[600],
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(
+                  child: Image.asset(
+                    'assets/lady.jpg',
+                  ),
+                  // backgroundImage: AssetImage('assets/lady.jpg'),
+                ),
+                accountName: Text("Akash Jindal"),
+                accountEmail: Text("akashjindal347@gmail.com"),
               ),
-            ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
+              // DrawerHeader(
+
+              //   child: Text('Drawer Header'),
+              //   decoration: BoxDecoration(
+              //     color: Colors.purple[600],
+              //   ),
+              // ),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Account'),
+                onTap: () async {
+                  await AuthService().signOut();
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.exit_to_app),
+                title: Text('Log Out'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
         ),
-      ),
         body: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             mainAxisSize: MainAxisSize.max,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              
               // RaisedButton(
               //   onPressed: () {
               //     Navigator.of(context).pushNamed('/tasks');
@@ -103,6 +133,69 @@ class HomeState extends State<HomeActivity> {
               //   },
               //   child: Text("logout"),
               // ),
+              Stack(
+                children: <Widget>[
+                  Container(
+                    height: 150,
+                    width: double.infinity,
+                    color: Colors.purple,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 200,
+                    padding: EdgeInsets.only(
+                      top: 80,
+                    ),
+                    child: Card(
+                      child: Stack(
+                        children: <Widget>[
+                          Center(
+                            child: Container(
+                              transform:
+                                  Matrix4.translationValues(0.0, -60.0, 0.0),
+                              width: 100,
+                              height: 100,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                image: new DecorationImage(
+                                  image: ExactAssetImage('assets/lady.jpg'),
+                                  fit: BoxFit.fitHeight,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 10),
+                            child: Center(
+                              child: Text(
+                                "G.I. Joe",
+                                style: TextStyle(
+                                    color: Colors.black87,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 40),
+                            child: Center(
+                              child: Text(
+                                "Designation",
+                                style: TextStyle(
+                                    color: Colors.black87,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      margin: EdgeInsets.symmetric(horizontal: 60),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
               Expanded(
                 child: StreamBuilder<List<Task>>(
                   builder: (context, snapshot) {
@@ -110,13 +203,13 @@ class HomeState extends State<HomeActivity> {
                       //print(snapshot.data.length);
                       print(snapshot.data.length);
                     }
-                    
+
                     return snapshot.data != null
                         ? ListView.builder(
                             itemBuilder: (context, index) {
-
                               return Card(
-                                margin: const EdgeInsets.all(10.0),
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 25),
                                 //margin: EdgeInsets.symmetric(horizontal: 20.0),
                                 elevation: 1.5,
                                 child: ListTile(
@@ -128,10 +221,15 @@ class HomeState extends State<HomeActivity> {
                                     }));
                                   },
                                   leading: CircleAvatar(
-                                    child: Text(snapshot.data[index].name),
+                                    backgroundColor: Colors.purple[500],
+                                    foregroundColor: Colors.white,
+                                    child: Text("#1"),
                                   ),
                                   title: Text(snapshot.data[index].description),
                                   subtitle: Text("date"),
+                                  trailing: Icon(
+                                    Icons.arrow_forward,
+                                  ),
                                 ),
                               );
                             },
