91e70a2cbfc7f8a117d1d9356e97dc702f8dabd5
BabliSah <BabliSah@github.com>
2020-07-25 15:35:57 +0530

    Basic Home Page

---
+++ mobile-app/team33_umeed/lib/activities/homepage.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomeActivity extends StatefulWidget{
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<HomeActivity>{
+    Widget _container(){
+      var container = Container(
+            margin: const EdgeInsets.all(10.0),
+            color: Colors.pink[900],
+            width: 100.0,
+            height: 100.0,
+          );
+        return
+        Container(
+        color: Colors.white30,
+        child: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: 1.0,
+          padding: const EdgeInsets.all(2.0),
+          mainAxisSpacing: 2.0,
+          crossAxisSpacing: 2.0,
+          children:[
+            container,
+            container,
+            container,
+            container,
+          ]),
+        ); 
+    }
+
+    Widget build(BuildContext context){
+      return Scaffold(
+            appBar: AppBar(
+              title: Text('Umeed'),
+              
+            ),
+            body: _container()       );
+    }
+}
