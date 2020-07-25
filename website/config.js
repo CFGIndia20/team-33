dbc0f893f5ef76919d42e29f3e31764d78ee3b54
miral25 <miral.gandhi4@gmail.com>
2020-07-25 19:16:35 +0530

    changes in navbar and added pages

---
+++ website/config.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC84lY9Ep2ozXamX1Ck2C62DmkBwQFPqrw",
+    authDomain: "team33-app.firebaseapp.com",
+    databaseURL: "https://team33-app.firebaseio.com",
+    projectId: "team33-app",
+    storageBucket: "team33-app.appspot.com",
+    messagingSenderId: "691086175931",
+    appId: "1:691086175931:web:24594ab5c4a02dfb5e439e",
+    measurementId: "G-YYYFXY6EDX"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
