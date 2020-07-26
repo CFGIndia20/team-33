5311623bee72e090723366438df51215a7301bfd
BabliSah <BabliSah@github.com>
2020-07-26 05:50:35 +0530

    log

--- website/js/tasks.js
+++ website/js/tasks.js
@@ -1,8 +1,6 @@
 function tasks(){
     firebase.database().ref('tasks').once('value',   function(snapshot) {
         snapshot.forEach(function(childSnapshot) {
-        //   var childKey = childSnapshot.key;
-        //   var childData = childSnapshot.val();
         document.getElementsByName('Tasks').innerHtml=childSnapshot.val();
         });
       });
